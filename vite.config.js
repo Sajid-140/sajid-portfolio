7a6e1f267730bd1cd077fbd55d3cd4fb4399791f
Sajid-140 <21011519-140@uog.edu.pk>
2025-06-12 14:53:31 +0500

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import tailwindcss from 'vite-plugin-tailwindcss'
 
 export default defineConfig({
-  // plugins: [react(), tailwindcss()],
+  base: '/sajid-portfolio/',
   plugins: [react()],
 })
